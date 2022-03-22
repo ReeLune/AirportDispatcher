f2bed09c956fbc86c7077ce903529bb8bf8b02ae
Reelune <2piroshok@mail.ru>
2022-03-22 21:49:53 +0500

    Добавление библиотеки стилей Изменение дизайна Попытка вывода информации из БД

--- AirportDispatcher/Pages/RegistrationPassengerPage.xaml.cs
+++ AirportDispatcher/Pages/RegistrationPassengerPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AirportDispatcher.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,27 @@ namespace AirportDispatcher.Pages
             //Подключение БД к ListView
             arrayFlight = db.context.Flight.ToList();
             AirlineListView.ItemsSource = arrayFlight;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            AirportEntities obj = new AirportEntities();
+            //DbSet<Flight> FlightTable 
+            //{
+            //    string Number { get; set;}
+            //};
+            var query =
+                from Flight in obj.Flight
+                join Airline in obj.Airline on Flight.Id_Name_Airline equals Airline.Id_Airline
+                join Airport_Name in obj.Airport_Name on Flight.Airport_From equals Airport_Name.Id_Airport
+                select new { Flight.Number_Flight, Airline.Airline_Name, Flight.Departure_Date, Flight.Count_Place_Remains, Airport_Name.Airport_Name1};
+            if (query.Count()!=0)
+            {
+                //FlightTable.Add(new Flight()
+                //{
+
+                //});
+            }
         }
     }
 }
